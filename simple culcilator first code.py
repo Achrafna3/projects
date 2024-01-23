5bfae71eb8db2f46f748306afaabe79996cb633f
Achrafna3 <77066238+Achrafna3@users.noreply.github.com>
2024-01-23 11:28:54 +0100

    Add files via upload

---
+++ simple culcilator first code.py
@@ -0,0 +1,13 @@
+num =float.input("num1")
+num2 =float.input("num2")
+opreation =input("chos")
+if opreation == "+":
+    print(num+num2)
+if opreation == "-":
+     print(num-num2)
+if opreation == "/":
+    print (num/num2)
+if opreation == "*":
+     print (num*num2)
+else:
+   print("wrong")
